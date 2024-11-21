75319bec02b128aef9b5bca3128b7c8b55fe2745
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-11-21 13:24:43 +0000

    installed required files and froze to requirements.txt

--- mantisfeed/settings.py
+++ mantisfeed/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#_kn28pqf^9)@6(5bh!kqhy3x6b##=_g960%+59s%(8-1cx5c)'
+SECRET_KEY = 'mantisfeed/settings.py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
