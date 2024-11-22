ad5260b5da480e44981cbf5f67ecf12dbcb5e9a8
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-11-22 13:54:17 +0000

    setup settings for herkou

--- mantisfeed/settings.py
+++ mantisfeed/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'mantisfeed/settings.py'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    ".herokuapp.com"
+    ]
 
 
 # Application definition
@@ -129,7 +132,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+#Media files config
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
