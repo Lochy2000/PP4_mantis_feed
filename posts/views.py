8a71f8803bcceed1d7834048e3a453575f86427d
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-11-27 12:41:42 +0000

    added views.py for post_list and post_create. Next create post_detail

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import listView, DetailView, CreateView,UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTextMixin
+from django.contrib.auth.decorators import login_required
+from django.http import JsonRepsonse
+from django.urls import reverse_lazy, reverse
+from django.contrib import messages 
+from .models import Posts, comment
+from django.db.models import count
 
 # Create your views here.
+
+@login_required 
+def Post_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        if not all ([title.content]):
+            messages.error(request,"Please fill in all fields.")
+            return redirct('posts:post_create')
+
+        try: 
+            post = Post.objects.create(
+                title=title,
+                content=content,
+                author=request.user
+            )
+            messges.success(request, "Post created successfully!")
+            return redirect('posts:post_detail', post_id=post.id)
+        except Exception as e:
+            messages.error(request, f"Error creating post: {str(e)}")
+            return redirct('posts:post_create')
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+
+    top_posts = Post.objects.annotate(
+        total_score = Count('upvotes') - Count('downvotes')
+    ).order_by('-total_score')[:5]
+
+    return render(request, 'posts/post_list.html',{
+        'posts' : posts,
+        'top_posts' : top_posts
+    })
