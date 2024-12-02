8505afebf3f6769c6c7081515f4b96014f03d73d
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-12-02 12:51:32 +0000

    registration form working

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegistrationForm
 
 
 # Create your views here.
 
 #---------- Login request ------------
+
+#-------- Registration -----------
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}! You can now log in.')
+            return redirect ('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'accounts/register.html',{'form': form})
+
--- mantisfeed/urls.py
+++ mantisfeed/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('posts.urls')),
     path ('accounts/login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path ('accounts/register/', accounts_views.register, name='register'),
     path ('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
