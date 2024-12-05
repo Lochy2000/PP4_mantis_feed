2710d8f300a412ec4cf46da9b65e585df135d3a2
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-12-05 17:32:24 +0000

    testing a news feed side bar added html

--- mantisfeed/settings.py
+++ mantisfeed/settings.py
@@ -27,6 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'mantisfeed/settings.py'
 
+#News API
+NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 ALLOWED_HOSTS = [
