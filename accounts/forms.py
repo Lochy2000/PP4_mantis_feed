81f0999f9b335a85c9f5901d944dcc854ba5d60f
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2024-12-09 12:04:28 +0000

    profile function added to accounts/views.py. Added urls to main urls.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    bio = forms.CharField(widget=forms.Textarea, requred=False)
+    profile_picture = forms.ImageField(required=False)
 
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['bio', 'profile_picture']
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm, UserProfileForm
 
 
 # Create your views here.
@@ -20,3 +21,19 @@ def register(request):
         form = UserRegistrationForm()
     return render(request, 'accounts/register.html',{'form': form})
 
+#-------- User Profile -----------
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
+
+@login_required 
+def edit_profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile has been updated!')
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=request.user.userprofile)
+    return render(request,'accounts/edit_profile.html', {'form':form})
